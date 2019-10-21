2c3869c67bc2266a602154bfd2d8dddea9b5c751
ezii <manu@korfmann.info>
2019-10-21 23:47:55 +0200

    ……

--- chat-bot-1.rb
+++ chat-bot-1.rb
@@ -1,3 +1,6 @@
+bot = nil
+
+
 def §(*args)
   yield
 end
@@ -12,6 +15,11 @@ LAW_SET = [
   TEST____ONLY____FETCHING_5_RESULTS_FROM_OTHER_BOT_USING_STATIC_QUERY,
   TEST_COMMAND_STRING = "5 results for test",
   ERR_CODE_0 = "search string for gbot is not a string"
+  ERR_CODE_1 = "respoonse not delivered within 1 second"
+
+
+
+  NOT_SUITABLE_FOR_STAGING_WHEN = {ERR_CODE_1: "happens for over 2% of test cases"}
 ]
 
 # TEST____ONLY____FETCHING_5_RESULTS_FROM_OTHER_BOT_USING_STATIC_QUERY = 0
@@ -19,6 +27,10 @@ LAW_SET = [
 
 # Manuel Arno Korfmann signature §set = law set = testing discord bots for communicating wiith other bots
 
+
+def split_into_pipe_parts(str)
+  str.split('|')
+end
 
 
 §(TEST____ONLY____FETCHING_5_RESULTS_FROM_OTHER_BOT_USING_STATIC_QUERY) do
@@ -29,7 +41,7 @@ LAW_SET = [
     end
     
     def to_discord_message
-      fail "ERR_CODE: 0" if @text.is_a?(String).false?
+      # fail "ERR_CODE: 0" if @text.is_a?(String).false?
       @text.to_s
     end
   end
@@ -42,20 +54,14 @@ LAW_SET = [
 
   bot = Discordrb::Commands::CommandBot.new token: ENV['BOT_TOKEN'], prefix: '!' do
 
-    bot.command :"gbot:" do |event|
-      # return event.user.name
-      
-      return GbotCommandForBot2Bot.new(TEST_COMMAND_STRING).to_discord_message
-    
-    
+    bot.command :"pipeline" do |event|
+      # return event.user.sname    
+      message = "gbot test | filter by google page speed score | top 1"
       pipe_parts = split_into_pipe_parts(message, '|')
+
+      return pipe_parts.inspect
   
       pipe_parts.each do |pipe_part| pipeline.add(pipe_part) end
-    end
-
-
-
-    def wait_for_response(starts_with: nil)
     end
   
   end
@@ -66,7 +72,6 @@ LAW_SET = [
   end
 
 
-  bot.run
 
 
   class Pipeline
@@ -85,4 +90,90 @@ LAW_SET = [
       end
     end
   end
+
+
+  module SendMessageAndProcessBotResponseWithin5Seconds
+    module Pipeline;end
+    class Pipeline::ExpectResponseWithin
+    end
+
+    class Pipeline::FailIfResponseCantBeDeliveredWithin1Second
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+puts GbotCommandForBot2Bot.new(TEST_COMMAND_STRING).to_discord_message
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bot.run
